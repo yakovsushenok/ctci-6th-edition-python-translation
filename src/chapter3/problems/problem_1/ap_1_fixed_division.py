bc5c262b48f598b971244c8c8fc19532b5491396
yakovsushenok <yakovsushenok@protonmail.com>
2023-07-25 18:18:37 -0400

    Finish chapter 3, problem 1, approach 1

--- src/chapter3/problems/problem_1/ap_1_fixed_division.py
+++ src/chapter3/problems/problem_1/ap_1_fixed_division.py
@@ -6,6 +6,13 @@ class FullStackException(Exception):
         super().__init__()
 
 
+# TODO: Put this into a separate file, duplicating code
+class EmptyStackException(Exception):
+    def __init__(self):
+        super().__init__()
+        self.message = "Empty Stack Exception"
+
+
 class FixedMultiStack:
     def __init__(self, stack_size: int) -> None:
         self.number_of_stacks = 3
@@ -14,10 +21,31 @@ class FixedMultiStack:
         self.sizes: List[int] = [0] * self.number_of_stacks
 
     def push(self, stack_num: int, value: int) -> None:
+        """
+        Note that stack_num starts from 0. stack_num is the stack we want to push to.
+        """
         if self.is_full(stack_num):
             raise FullStackException
         self.sizes[stack_num] += 1
         self.values[self.index_of_top(stack_num)] = value
+
+    def pop(self, stack_num: int) -> int:
+        if self.is_empty(stack_num):
+            raise EmptyStackException
+
+        top_index = self.index_of_top(stack_num)
+        value = self.values[top_index]
+        self.values[top_index] = 0
+        self.sizes[stack_num] -= 1
+        return value
+
+    def peek(self, stack_num: int) -> int:
+        if self.is_empty(stack_num):
+            raise EmptyStackException
+        return self.values[self.index_of_top(stack_num)]
+
+    def is_empty(self, stack_num: int) -> bool:
+        return self.sizes[stack_num] == 0
 
     def is_full(self, stack_num: int) -> bool:
         return self.sizes[stack_num] == self.stack_capacity
--- src/chapter3/problems/problem_1/ap_1_fixed_division_test.py
+++ src/chapter3/problems/problem_1/ap_1_fixed_division_test.py
@@ -1,5 +1,5 @@
 import unittest
-from ap_1_fixed_division import FixedMultiStack
+from ap_1_fixed_division import FixedMultiStack, FullStackException, EmptyStackException
 
 
 class MyQueueTests(unittest.TestCase):
@@ -7,8 +7,50 @@ class MyQueueTests(unittest.TestCase):
         self.fixed_multi_stack = FixedMultiStack(4)
 
     def test_push(self):
-        # When we call push, and the corresponding stack is not full, 
-        self.fixed_multi_stack.push(2)
+        # When we call push, and the corresponding stack is not full, we expect the value to push to the
+        # beginning of the stack
+        stack_to_push_to = 1  # pushing to the second stack (since they start from 0)
+        value_to_push = 34
+        self.fixed_multi_stack.push(stack_to_push_to, value_to_push)
+        expected_stack_values = [0, 0, 0, 0, 34, 0, 0, 0, 0, 0, 0, 0]
+        self.assertEqual(self.fixed_multi_stack.values, expected_stack_values)
+        # and the size of the corresponding stack should increase by 1
+        self.assertEqual(1, self.fixed_multi_stack.sizes[stack_to_push_to])
+        # When we call push and the corresponding stack is full, we expect it to raise an exception
+        self.fixed_multi_stack.push(stack_to_push_to, 2)
+        self.fixed_multi_stack.push(stack_to_push_to, 3)
+        self.fixed_multi_stack.push(stack_to_push_to, 4)
+        self.assertRaises(
+            FullStackException, lambda: self.fixed_multi_stack.push(stack_to_push_to, 5)
+        )
+
+    def test_pop(self):
+        # When we call pop, and the corresponding stack is empty, we expect it to raise an exception
+        self.assertRaises(EmptyStackException, lambda: self.fixed_multi_stack.pop(0))
+        # When we call pop, and the corresponding stack is not empty, we should expect it to return
+        # the value at the top of the stack
+        value_to_be_popped = 12
+        stack_to_pop_from = 0
+        self.fixed_multi_stack.push(
+            stack_to_pop_from, value_to_be_popped
+        )  # first add a value to the stack
+        self.assertEqual(
+            value_to_be_popped, self.fixed_multi_stack.pop(stack_to_pop_from)
+        )
+        # and the size of the corresponding stack should decrease by 1
+        self.assertEqual(0, self.fixed_multi_stack.sizes[stack_to_pop_from])
+        # and the value should have been deleted from the stack
+        expected_stack_values = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.assertEqual(expected_stack_values, self.fixed_multi_stack.values)
+
+    def test_peak(self):
+        # When we call peek, and the stack is empty, it should raise an exception
+        self.assertRaises(EmptyStackException, lambda: self.fixed_multi_stack.peek(0))
+        # When we call peek, and the stack is not empty, it should return the value at the top of the
+        # corresponding stack
+        self.fixed_multi_stack.push(0, 1)
+        self.fixed_multi_stack.push(0, 2)
+        self.assertEqual(self.fixed_multi_stack.peek(0), 2)
 
 
 if __name__ == "__main__":
